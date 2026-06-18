cf09a73141142b773d09e86933e334db3814b90d
Enzo <oktank149@gmail.com>
2026-06-19 02:55:25 +0700

    init

---
+++ src/main/java/com/smarted/ed/SmartEdBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.smarted.ed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEdBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartEdBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smarted/ed/SmartEdBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarted.ed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEdBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
